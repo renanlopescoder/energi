73d296c0eabbaa381034287754a6b78b64bc576e
Renan Lopes <renanlopescoder@gmail.com>
2021-09-25 14:31:25 +0200

    bugfix/signin-metamask

--- src/components/SigninButton/index.jsx
+++ src/components/SigninButton/index.jsx
@@ -3,20 +3,10 @@ import { Button } from "antd";
 import { useMoralis } from "react-moralis";
 
 import { LogoutOutlined } from "@ant-design/icons";
-import { useRouter } from "next/router";
 
 export default function SigninButton({ title = "Signin" }) {
-  const { authenticate, logout, isAuthenticated, user, isAuthenticating } =
+  const { authenticate, logout, isAuthenticated, isAuthenticating } =
     useMoralis();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (user) {
-      if (!user.attributes.email) {
-        router.push("/signup");
-      }
-    }
-  }, [user, router]);
 
   return (
     <>
